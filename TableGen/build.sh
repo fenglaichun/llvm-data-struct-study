daf7a7f11f2fc1fac8a336f8da57e821bb35097e
fenglaichun <fenglaichun@126.com>
2021-09-20 15:45:42 +0800

    [tblgen]add the demo td file.

--- TableGen/build.sh
+++ TableGen/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 g++ `llvm-config --cxxflags` TableGen.cpp -c -o TableGen.o
 g++ `llvm-config --ldflags` -o main TableGen.o `llvm-config --libs support tablegen` -lpthread -ltinfo
+./main class_def.td
